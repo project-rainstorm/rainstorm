532d3d819646f08883d8851bde6d314120904f3d
William The Hippo <nunya@example.com>
2020-09-11 01:14:44 -0700

    merge react and flask

--- thunder/src/App.js
+++ thunder/src/App.js
@@ -1,5 +1,5 @@
 import "babel-polyfill";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from './style.css';
 import Tabs from "./components/Tabs";
 // pages
@@ -7,18 +7,20 @@ import Market from "./pages/Market";
 import NewJob from "./pages/NewJob";
 
 function App() {
-  const tabList = [
-    { name: "market", label: "Marketplace", content: Market },
-    { name: "newjob", label: "New Job", content: NewJob }
-  ];
+  const [services, setServices] = useState(null);
+
+  useEffect(() => {
+    fetch('/services').then(res => res.json()).then(data => {
+      setServices(data.data)
+    });	  
+  }, []);
+
   return (
     <div className={style.app}>
-      <Tabs tabList={tabList} />
-      <header className={style.appHeader}>
-        <p>
-          <code>p2print</code>
-        </p>
-      </header>
+      {services && services.map((service) => (
+        <div>{service.name}</div>
+       ))
+      }
     </div>
   );
 }

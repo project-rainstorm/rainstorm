1346230f9a3816ab278d2f32952d2631b027b1bc
Count Null <fuck@off.lol>
2020-09-23 20:46:58 +0100

    add: transmission change password

--- scripts/pw_change.sh
+++ scripts/pw_change.sh
@@ -25,6 +25,10 @@ echo "$DEFAULT_USER:$PASSWORD" | sudo chpasswd
 # Save hashed password
 echo "$HASH_SHA256" > $NEW_PW_HASH_FILE
 
+# Change transmission password
+sed -i '/PASS/d' $PROJECT_DIR/services/transmission/.env
+echo "PASS=${PASSWORD}" >> $PROJECT_DIR/services/transmission/.env
+
 # Change Restic backup repo password
 # NOTE: restic needs both the old and new password in a file
 #       for restic, we use the hash of the password as the password

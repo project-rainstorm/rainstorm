241757abdcf9c1de0cc175147efe6059a5ca6476
William The Hippo <nunya@example.com>
2020-09-24 13:56:39 -0700

    accept json as body

--- raincloud/__init__.py
+++ raincloud/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask import request
 from raincloud.models.service import Service
 from raincloud.models.system import SystemStatus
 from flask_json import FlaskJSON, as_json
@@ -107,6 +108,15 @@ def create_app(test_config=None):
             return {'status': 'failed'}
         else:
             return { 'data': service.__dict__ }
+
+    @app.route('/services/<service_name>/vars', methods=['POST'])
+    @as_json
+    def vars(service_name):
+        if request.is_json:
+            req = request.get_json()
+            print(req)
+
+            return req
 
     @app.route('/settings/system/info', methods=['GET'])
     @as_json

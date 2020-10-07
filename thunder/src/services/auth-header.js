2b088a3c5ee48bc878acb82d24adf0d5dbb0afb2
William The Hippo <nunya@example.com>
2020-10-07 00:33:35 -0700

    get login working on front page

---
+++ thunder/src/services/auth-header.js
@@ -0,0 +1,9 @@
+export default function authHeader() {
+	const user = JSON.parse(localStorage.getItem('user'));
+
+	if (user && user.access_token) {
+		return { Authorization: `jwt ${user.access_token}` };
+	}
+	
+	return {};
+}
---
+++ thunder/src/services/auth.service.js
@@ -0,0 +1,34 @@
+class AuthService {
+  login(username, password) {
+    return fetch('/auth', {
+      method: 'post',
+      headers: {
+				Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+			body: JSON.stringify({
+				username: username,
+        password: password
+			})
+    })
+      .then((res) => res.json())
+      .then((data) => {
+				console.log('auth service', data);
+        if (data.access_token) {
+          localStorage.setItem('user', JSON.stringify(data));
+        }
+
+        return data.data
+      });
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem('user'));
+  }
+}
+
+export default new AuthService();

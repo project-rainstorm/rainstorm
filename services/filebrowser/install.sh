4b8961fdccaa54994966c12602024a6000961320
count-null <70529195+count-null@users.noreply.github.com>
2020-10-11 00:31:42 +0100

    add:comments

--- services/filebrowser/install.sh
+++ services/filebrowser/install.sh
@@ -2,6 +2,7 @@
 
 SERVICE_NAME=filebrowser
 
+# When docker mounts a file, the file must exist.
 mkdir -p /mnt/usb/apps/$SERVICE_NAME/
 touch /mnt/usb/apps/$SERVICE_NAME/.filebrowser.json
 touch /mnt/usb/apps/$SERVICE_NAME/database.db

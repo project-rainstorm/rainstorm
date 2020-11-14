98f215628a1a53e929dfd9aa14084abd13a5b2c9
William The Hippo <nunya@example.com>
2020-11-14 08:05:58 -0800

    don't hardcode username

--- scripts/setup_production.sh
+++ scripts/setup_production.sh
@@ -3,6 +3,9 @@ sudo apt-get install nginx
 scripts_dir="$(dirname "$(readlink -f "$0")")"
 root_dir="$(dirname "$(readlink -f $scripts_dir)")"
 
+source "$scripts_dir"/functions.sh
+eval $(_parse $root_dir/config.yml)
+
 yarn --cwd "$root_dir/thunder" run build
 
 if [ -d "$root_dir/venv" ]; then
@@ -25,6 +28,7 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo cp "$scripts_dir/raincloud.service.example" "$scripts_dir/raincloud.service"
 
 sed -i "s~<root_dir>~$root_dir~g" $scripts_dir/raincloud.service
+sed -i "s~<user>~$default_username~g" $scripts_dir/raincloud.service
 
 sudo mv "$scripts_dir/raincloud.service" /etc/systemd/system/raincloud.service
 
